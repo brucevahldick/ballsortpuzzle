ffd7a7bda16d3bcdc8f5141f28c5c0cc057f0f1a
Bruce Vahldick <brucev.br@gmail.com>
2022-04-28 13:33:35 -0300

    fix: organização da estrutura

--- visao/Puzzle.java
+++
@@ -1,7 +0,0 @@
-package visao;
-
-public interface Puzzle {
-    public abstract void addBall(String r, int x, int y);
-    public abstract void clearMainPanel();
-    public abstract void toggleView(boolean stage);
-}
